cf5bd88f13702cb715ce119a3e9068bfc6a174bb
Eugene <eugenepomevor@gmail.com>
2024-12-23 23:58:45 +0000

    state deployment to s3 bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,8 +3,13 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "5.82.2"
-    }
+    } 
   }
+    backend "s3" {
+    bucket = "project203"
+    key = "devops/infrastructure/s3.tfstate"
+    region = "us-west-2"
+  }   
 }
 
 provider "aws" {
