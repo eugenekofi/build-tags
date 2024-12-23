56870f49be0590752fc779e2e5e901fcac15b1b0
Eugene <eugenepomevor@gmail.com>
2024-12-23 16:33:37 +0000

    deploy lambda function

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "pek-lambda1"
+  description   = "Pek lambda function"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+
+  source_path = ["${path.module}/src/lambda-functions"]
+  tags = {
+    Name = "pek-lambda1"
+  }
+}
